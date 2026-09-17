9e02497ff8d91fc6dcf4218077f9dbeef0fc8f1b
Chetan Kumar Sambhawani <b230890@skit.ac.in>
2026-09-17 09:45:23 +0000

    chore: initialize Spring Boot project

---
+++ src/main/java/com/parkease/ParkeaseApplication.java
@@ -0,0 +1,12 @@
+package com.parkease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkeaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkeaseApplication.class, args);
+    }
+}
